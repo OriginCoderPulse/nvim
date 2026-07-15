6a5584d85c1ebadde326db41227923382b41b6a9
Brain <brain@BraindeMac-mini.local>
2026-07-15 15:09:55 +0800

    refactor(flash): bind jump keymap via Pack.var

--- lua/packages/configs/flash.lua
+++ lua/packages/configs/flash.lua
@@ -4,11 +4,17 @@ Pack.register({
 }):load({
 	event = "BufReadPost",
 	once = true,
+	var = {
+		jump = {
+			use = true,
+			callback = function(plugin)
+				vim.keymap.set({ "n", "x", "o" }, "f", function()
+					plugin.jump()
+				end, { desc = "Flash" })
+			end,
+		},
+	},
 	config = function(plugin)
 		plugin.setup({})
-
-		vim.keymap.set({ "n", "x", "o" }, "f", function()
-			plugin.jump()
-		end, { desc = "Flash" })
 	end,
 })
